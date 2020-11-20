1adfd3c08c380a138f85ab341f1adc43a7f05e62
Jeff Golden <gjgolden08@gmail.com>
2020-11-20 00:11:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,9 +16,9 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
-def assembled_matrix = [
-  CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE
-]
+def assembled_matrix 
+  [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+
   
   
   
